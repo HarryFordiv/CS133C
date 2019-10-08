2cbdaf3d74611195da3729a1efaa15c884044481
HarryFordiv <56173360+HarryFordiv@users.noreply.github.com>
2019-10-07 23:19:32 -0700

    Add files via upload

---
+++ A1/Basics.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//assignment 1: make a program that defines and prints out the basic variable types available in c.
+/*
+main
+variable definitions
+print statements
+*/
+void main()
+{
+	int a = 3;
+	long b = 0l;
+	char c = 'H';
+	float d = 1.1;
+
+	printf("\n%c%d%f%ld",c,a,d,b);
+	printf (" world!\n");
+	
+
+}
